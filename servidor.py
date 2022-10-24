a4b351d373429f86a9c35671bb07b58eeb31367e
RC0ndado <81990698+RC0ndado@users.noreply.github.com>
2022-10-24 11:37:33 -0500

    servidorFlask v1

--- servidor.py
+++ servidor.py
@@ -1,4 +1,5 @@
 from crypt import methods
+from distutils.log import debug
 from flask import Flask, request, jsonify, render_tem
 
 
@@ -8,3 +9,9 @@ servidorWeb = Flask(__name__)
 
 #Anotación
 @servidorWeb.route("/test", methods=['GET'])    # Esto es como declarar un app.js
+def formulario():
+    return render_template('pagina.html')
+
+if __name__ == '__main__':
+    servidorWeb.run(debug=False, host='0.0.0.0', port='8080')    #Por si se quiere conectar a cualquier zona, pero se tiene que poner 
+                                                                #EL puerto que se desea conecatr
